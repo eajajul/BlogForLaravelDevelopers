938d5be0ae01c5884bb5fa010b087d87d7d57136
Haque Md EaJaJuL <38464225+eajajul@users.noreply.github.com>
2019-05-27 23:56:41 +0600

    Day 3 wrap up

---
+++ Blog/resources/views/Pages/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.app')
+   @section('content')
+  
+     <div class=".container-fluid text-center ">
+
+   <h1 class="display-4" >Welcome to laravel</h1>
+   <p class="lead">This is the Blog Home</p>
+    <hr class="my-4">
+   <button type="button" class="btn btn-primary">Log In</button>
+   <button type="button" class="btn btn-primary">SignUp</button>
+</div>
+
+   @endsection
+    
